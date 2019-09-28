310f3ef95d76606872ff2432f3aacd63d40b8489
VBTX <jv.svedaite@gmail.com>
2019-09-28 10:43:08 -0500

    add markdown pages and edit some initial set up for practice

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my blog where I write about things that I am learning.</p>
+    <p>These are my notebooks, basically.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/jingle-bells/">Go to Jingle Bells page</Link>
   </Layout>
 )
 
